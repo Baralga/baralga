7b0eaa8108c16a1f4e9da6e5c082732d0787d830
remast <remast@9ccbd0ec-8b40-4f08-9906-0ef8eb69c84a>
2008-04-05 13:37:51 +0000

    Upgrade auf neue Versionen

--- application/trunk/baralga-core/src/main/java/org/remast/gui/util/Constants.java
+++ application/trunk/baralga-core/src/main/java/org/remast/gui/util/Constants.java
@@ -5,38 +5,43 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 
-import org.jdesktop.swingx.decorator.AlternateRowHighlighter;
+import org.jdesktop.swingx.decorator.ColorHighlighter;
+import org.jdesktop.swingx.decorator.HighlightPredicate;
 import org.jdesktop.swingx.decorator.Highlighter;
-import org.jdesktop.swingx.decorator.RolloverHighlighter;
+import org.jdesktop.swingx.decorator.HighlighterFactory;
 
 /**
  * @author remast
  */
 public interface Constants {
 
-    //------------------------------------------------
+    // ------------------------------------------------
     // Date Formats
-    //------------------------------------------------
-    
+    // ------------------------------------------------
+
     public static final SimpleDateFormat hhMMFormat = new SimpleDateFormat("HH:mm"); //$NON-NLS-1$
+
     
-    //------------------------------------------------
+    // ------------------------------------------------
     // Number Formats
-    //------------------------------------------------
+    // ------------------------------------------------
 
     public static final NumberFormat durationFormat = new DecimalFormat("#0.00"); //$NON-NLS-1$
+
     
-    //------------------------------------------------
+    // ------------------------------------------------
     // Colors
-    //------------------------------------------------
+    // ------------------------------------------------
 
     public static final Color BEIGE = new Color(245, 245, 220);
-    
+
     public static final Color VERY_LIGHT_GREY = new Color(240, 240, 240);
-    
+
     public static final Color DARK_BLUE = new Color(64, 64, 128);
 
-    public static Highlighter[] HIGHLIGHTERS = new Highlighter[] { AlternateRowHighlighter.beige,
-    new RolloverHighlighter(Color.DARK_GRAY, Color.WHITE) };
+    public static Highlighter[] HIGHLIGHTERS = new Highlighter[] { 
+        HighlighterFactory.createSimpleStriping(BEIGE),
+        new ColorHighlighter(HighlightPredicate.ROLLOVER_ROW, Color.DARK_GRAY, Color.WHITE)
+    };
 
 }
