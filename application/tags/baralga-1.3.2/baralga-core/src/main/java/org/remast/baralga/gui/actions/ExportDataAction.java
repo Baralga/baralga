1b5c1aaf75feca63da7676a58d68ab347f93086c
remast <remast@9ccbd0ec-8b40-4f08-9906-0ef8eb69c84a>
2009-02-10 14:52:00 +0000

    Created tag baralga-1.3.2.

---
+++ application/tags/baralga-1.3.2/baralga-core/src/main/java/org/remast/baralga/gui/actions/ExportDataAction.java
@@ -0,0 +1,83 @@
+package org.remast.baralga.gui.actions;
+
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.remast.baralga.gui.model.PresentationModel;
+import org.remast.baralga.model.io.ProTrackWriter;
+import org.remast.swing.util.FileFilters;
+import org.remast.util.TextResourceBundle;
+
+/**
+ * Action to export data to a data file.
+ * @author remast
+ */
+@SuppressWarnings("serial") //$NON-NLS-1$
+public class ExportDataAction extends AbstractBaralgaAction {
+    
+    /** The logger. */
+    private static final Log log = LogFactory.getLog(ExportDataAction.class);
+
+    /** The bundle for internationalized texts. */
+    private static final TextResourceBundle textBundle = TextResourceBundle.getBundle(ExportDataAction.class);
+
+    public ExportDataAction(final Frame owner, final PresentationModel model) {
+        super(model);
+
+        putValue(NAME, textBundle.textFor("ExportDataAction.Name")); //$NON-NLS-1$
+        putValue(SHORT_DESCRIPTION, textBundle.textFor("ExportDataAction.ShortDescription")); //$NON-NLS-1$
+        putValue(SMALL_ICON, new ImageIcon(getClass().getResource("/icons/gnome-mime-text-xml.png"))); //$NON-NLS-1$
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void actionPerformed(final ActionEvent event) {
+        final JFileChooser chooser = new JFileChooser();
+        chooser.setFileFilter(new FileFilters.DataFileFilter());
+
+        int returnVal = chooser.showSaveDialog(getOwner());
+        if (JFileChooser.APPROVE_OPTION == returnVal) {
+            File file = chooser.getSelectedFile();
+            if (!file.getAbsolutePath().endsWith(textBundle.textFor("ExportDataAction.DataFileExtension"))) { //$NON-NLS-1$
+                file = new File(file.getAbsolutePath() + textBundle.textFor("ExportDataAction.DataFileExtension")); //$NON-NLS-1$
+            }
+
+            final ExportWorker exportWorker = new ExportWorker(getModel(), file);
+            exportWorker.execute();
+        }
+
+    }
+
+    private static class ExportWorker extends SwingWorker<String, Object> {
+        private PresentationModel model;
+        private File file;
+
+        public ExportWorker(final PresentationModel model, final File file) {
+            this.model = model;
+            this.file = file;
+        }
+        
+        @Override
+        public String doInBackground() {
+            try {
+                final ProTrackWriter writer = new ProTrackWriter(model.getData());
+                writer.write(file);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, textBundle.textFor("ExportDataAction.IOException.Message", file.getAbsolutePath()), textBundle.textFor("ExportDataAction.IOException.Heading"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                        JOptionPane.ERROR_MESSAGE);
+                log.error(e, e);
+            }
+            return null;
+        }
+    }
+}
---
+++ application/tags/baralga-1.3.2/baralga-core/src/main/java/org/remast/util/TextResourceBundle.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package org.remast.util;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Provides easy access to texts from properties files. This uses the default 
+ * Java mechanisms but looks for properties files in the package of a class.
+ * This makes it easier to modularize texts in applications as texts are always
+ * bundled with the class.
+ * @author remast
+ */
+public class TextResourceBundle {
+
+    /** The name of the properties file. */
+    private static final String PROPERTIES_FILE_NAME = "Texts";
+
+    /** The logger. */
+    private static final Log log = LogFactory.getLog(TextResourceBundle.class);
+
+    /** The resource bundle. */
+    private ResourceBundle resourceBundle = null;
+
+    /** The class to resolve texts for. */
+    private Class clazz;
+
+    private TextResourceBundle(final Class clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("Parameter clazz may not be null.");
+        }
+        
+        this.clazz = clazz;
+
+        try {
+            resourceBundle = ResourceBundle.getBundle(clazz.getPackage().getName() + "." + PROPERTIES_FILE_NAME);
+        } catch (MissingResourceException e) {
+            log.error(e, e);
+        }
+    }
+
+    /**
+     * Get's the bundle to resolve texts.
+     * @param clazz the class to resolve bundle for
+     * @return the resolved text bundle
+     */
+    public static TextResourceBundle getBundle(final Class clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("Parameter clazz may not be null.");
+        }
+
+        final TextResourceBundle textBundle = new TextResourceBundle(clazz);
+        return textBundle;
+    }
+
+    /**
+     * Get internationalized string for the given key.
+     * @param key the key for the internationalization
+     * @return the resolved message
+     */
+    public String textFor(final String key) {
+        try {
+            if (resourceBundle == null) {
+                return '!' + key + '!';
+            }
+
+            return resourceBundle.getString(key);
+        } catch (MissingResourceException e) {
+            log.error("Could not resolve text for class " + clazz.getName() + " and key \""  + key + "\".", e);
+            return '!' + key + '!';
+        }
+    }
+
+    /**
+     * Get internationalized string for the given key and resolve arguments.
+     * @param key the key for the internationalization
+     * @param arguments the arguments to be resolved
+     * @return the resolved message
+     */
+    public String textFor(final String key, final Object... arguments) {
+        return MessageFormat.format(textFor(key), arguments);
+    }
+}
