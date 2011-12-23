4f08c8f359e8a8c2f90b40c0ede7037806c6bfb9
remast <remast@9ccbd0ec-8b40-4f08-9906-0ef8eb69c84a>
2011-12-23 15:59:26 +0000

    Cleanup

--- application/trunk/baralga-core/src/main/java/org/remast/swing/JSearchField.java
+++ application/trunk/baralga-core/src/main/java/org/remast/swing/JSearchField.java
@@ -1,11 +1,3 @@
-//---------------------------------------------------------
-// $Id$ 
-// 
-// (c) 2011 Cellent Finance Solutions AG 
-//          Calwer Strasse 33 
-//          70173 Stuttgart 
-//          www.cellent-fs.de 
-//--------------------------------------------------------- 
 package org.remast.swing;
 
 import javax.swing.ImageIcon;
