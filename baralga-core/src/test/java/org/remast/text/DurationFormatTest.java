ff61ab522005c01b60ab10c261baf9ecdb945bc7
Jan Stamer <jan.stamer@red6-es.de>
2014-12-08 08:13:45 +0100

    Avoid collision of test with user config.

--- baralga-core/src/test/java/org/remast/text/DurationFormatTest.java
+++ baralga-core/src/test/java/org/remast/text/DurationFormatTest.java
@@ -1,7 +1,10 @@
 package org.remast.text;
 
 import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.remast.baralga.gui.settings.UserSettings;
 import org.remast.baralga.model.ProjectActivity;
 import org.remast.util.DateUtils;
 
@@ -11,8 +14,16 @@ public class DurationFormatTest {
 
     private DurationFormat format = new DurationFormat();
 
+    private UserSettings.DurationFormat initialDurationFormat;
+
+    @Before
+    public void before() {
+        initialDurationFormat = UserSettings.instance().getDurationFormat();
+    }
+
     @Test
     public void formatDurationInHoursAndMinutes() {
+        UserSettings.instance().setDurationFormat(UserSettings.DurationFormat.HOURS_AND_MINUTES);
         ProjectActivity act;
         DateTime startTime = new DateTime(DateUtils.getNow());
 
@@ -30,6 +41,11 @@ public class DurationFormatTest {
 
         act = new ProjectActivity(startTime, startTime.plusHours(7).plusMinutes(15), null);
         assertEquals("07:15", format.format(act.getDuration()));
+    }
+
+    @After
+    public void after() {
+        UserSettings.instance().setDurationFormat(initialDurationFormat);
     }
 
 }
